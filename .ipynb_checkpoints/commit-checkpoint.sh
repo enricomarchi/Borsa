ad67183bdf020c9bde40d93b0a19fa44d8977395
Enrico Marchi <enricomarchi@hotmail.it>
2023-10-30 14:34:55 +0100

    agg

--- .ipynb_checkpoints/commit-checkpoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git config --global user.name "enricomarchi"
-git config --global user.email "enricomarchi@hotmail.it"
-git remote set-url origin https://enricomarchi@github.com/enricomarchi/Borsa.git
--- Installa_CUDA.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget https://developer.download.nvidia.com/compute/cuda/12.2.2/local_installers/cuda_12.2.2_535.104.05_linux.run
-sudo sh cuda_12.2.2_535.104.05_linux.run
